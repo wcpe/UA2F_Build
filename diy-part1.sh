4b386fc7ea571ae21af3b5f118749fd093d743d8
wxys233 <56812380+wcpe@users.noreply.github.com>
2022-03-27 08:42:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,21 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+
+# 添加SSRP+
+echo "src-git helloworld https://github.com/fw876/helloworld.git" >> feeds.conf.default
+
+# 添加OpenClash
+git clone https://github.com/vernesong/OpenClash.git
+cp -rf OpenClash/luci-app-openclash package/luci-app-openclash
+
+# 添加UA2F
+git clone https://github.com/Zxilly/UA2F.git package/UA2F
+
+# 更新queue
+git clone https://github.com/openwrt/packages
+rm -rf package/libs/libnetfilter-queue
+# cp命令方便本地使用
+cp -rf packages/libs/libnetfilter-queue package/libs/
